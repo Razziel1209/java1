271c9a562ea8a4750b6f8999e38ea0160601dd9e
RAZZIEL <bybl@hotmail.com>
2023-02-02 21:02:47 -0600

    Ejercicio 01

---
+++ src/com/ejercicio/ImprimirDatos.java
@@ -0,0 +1,30 @@
+package com.ejercicio;
+public class ImprimirDatos {
+
+    public static void main(String[] args){
+        //Enteros
+        byte a=1;
+        short b=56;
+        int c=96;
+        long d=234;
+        //Decimales
+        float e=5.67f;
+        double f=34.67d;
+        boolean g=false;
+        boolean h=true;
+        //Texto
+        char i='r';
+        String j="Hola razziel";
+        //Imprimir los datos
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
